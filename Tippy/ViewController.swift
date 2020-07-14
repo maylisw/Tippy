d5307d0529506a164fdcbb7744174cbebd2d833d
Maÿlis Whetsel <maylisw31@gmail.com>
2020-07-14 15:45:30 -0700

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Maÿlis Whetsel on 7/14/20.
+//  Copyright © 2020 Maÿlis Whetsel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
